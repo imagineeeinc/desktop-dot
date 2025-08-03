9bf0a45aff8e7e01ee5ce1df6d3363c0bf74748d
imagineeeinc <59028351+imagineeeinc@users.noreply.github.com>
2025-08-03 21:21:05 +0100

    hypr: added support for external display and projection settings

---
+++ hypr/.config/hypr/display_mode.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+op=$( echo -e "Screen1\nMirror\nExtend Above\nExtend Right\nExtend Left\nScreen2" | wofi -i --dmenu )
+
+case $op in
+  Screen1)
+    hyprctl --batch "keyword monitor eDP-1,preferred,0x0,1.0666667"
+    hyprctl --batch "keyword monitor HDMI-A-1,disable"
+    ;;
+  Mirror)
+    hyprctl --batch "keyword monitor eDP-1,preferred,0x0,1.0666667"
+    hyprctl --batch "keyword monitor HDMI-A-1,preferred,auto,1,mirror,eDP-1"
+    ;;
+  "Extend Above")
+    hyprctl --batch "keyword monitor eDP-1,preferred,0x0,1.0666667"
+    hyprctl --batch "keyword monitor HDMI-A-1,preferred,0x-1080,1"
+    ;;
+  "Extend Right")
+    hyprctl --batch "keyword monitor eDP-1,preferred,0x0,1.0666667"
+    hyprctl --batch "keyword monitor HDMI-A-1,preferred,1802x0,1"
+    ;;
+  "Extend Left")
+    hyprctl --batch "keyword monitor eDP-1,preferred,0x0,1.0666667"
+    hyprctl --batch "keyword monitor HDMI-A-1,preferred,-1920x0,1"
+    ;;
+  Screen2)
+    hyprctl --batch "keyword monitor eDP-1,disable"
+    ;;
+esac
--- hypr/.config/hypr/resolution_selector.sh
+++ hypr/.config/hypr/resolution_selector.sh
@@ -4,9 +4,9 @@ op=$( echo -e "Standard\n1920x1080" | wofi -i --dmenu )
 
 case $op in 
   Standard)
-    hyprctl --batch "keyword monitor ,preferred,auto,1.0666667,bitdepth,10,cm,auto"
+    hyprctl --batch "keyword monitor eDP-1,preferred,0x0,1.0666667,bitdepth,10,cm,auto"
     ;;
   1920x1080)
-    hyprctl --batch "keyword monitor ,1920x1080,auto,1,bitdepth,10,cm,auto"
+    hyprctl --batch "keyword monitor eDP-1,1920x1080,0x0,1,bitdepth,10,cm,auto"
     ;;
 esac
