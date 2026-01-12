ebe0827c7f3843c7833a533847d5354f333e210c
imagineeeinc <imagineee@tilde.green>
2026-01-12 01:05:02 +0000

    hypr: updated wallpaper switcher notification with new details

--- hypr/.config/hypr/wofi_wallpaper_selector.sh
+++ hypr/.config/hypr/wofi_wallpaper_selector.sh
@@ -82,14 +82,14 @@ if [ -n "$selected" ]; then
     if [ -n "$original_path" ]; then
         # Set wallpaper using swww with the original file
         # hyprctl hyprpaper reload , "$original_path"
-        awww img "$original_path" --transition-type=outer --transition-pos=bottom --transition-duration=2 --transition-fps=120 --transition-bezier=0.80,0,0.23,1
+        awww img "$original_path" --transition-type=outer --transition-pos=bottom --transition-duration=2 --transition-fps=120 --transition-bezier=0.80,0,0.23,1 --resize=crop
 
         # Save the selection for persistence
         echo "$original_path" > "$HOME/.cache/current_wallpaper"
 
         # Optional: Notify user
-        notify-send "Wallpaper" "Wallpaper has been updated" -i "$original_path"
+        notify-send "Wallpaper Change" "Wallpaper has been updated" -i "$original_path" -a "Wallpaper Switcher"
     else
-        notify-send "Wallpaper Error" "Could not find the original wallpaper file."
+        notify-send "Wallpaper Error" "Could not find the original wallpaper file." -a "Wallpaper Switcher"
     fi
 fi
